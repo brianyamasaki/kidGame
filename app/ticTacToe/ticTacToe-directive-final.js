ac15e7c40e7bea663954928aa069dfe11ff09ff1
Brian Yamasaki <brian.yamasaki@SeattleGT.com>
2016-02-21 16:29:09 -0800

    Created Description of Code and Exercises section and removed code

--- app/ticTacToe/ticTacToe-directive-final.js
+++ app/ticTacToe/ticTacToe-directive-final.js
@@ -30,7 +30,6 @@ angular.module('myApp.ticTacToeDirective', ['ngRoute'])
 
         // This function allows displaying of boardSquare content onto the TicTacToe board on the main page.
         // It is called by the Angular through ng-click="squareClass($index)" in the template HTML
-        // This shouldn't have to be modified for the exercises
         scope.squareClass = function(index){
           return (scope.boardSquares[index]);
         };
@@ -38,7 +37,17 @@ angular.module('myApp.ticTacToeDirective', ['ngRoute'])
         // Function is called when user clicks on a board square
         // index will be a value from 0 to 8
         scope.squareClick = function(index) {
-
+          // if the game is not won or tied, the value is undefined. If it's not undefined, then ignore this click.
+          if (scope.gameOver !== undefined) {
+            return;
+          }
+          // check if this square already has a value (x or o). If so, ignore this click
+          if (scope.boardSquares[index] !== undefined) {
+            scope.errorMsg = 'Cannot Click on this square';
+            return;
+          } else {
+            scope.errorMsg = undefined;
+          }
           // put the current player into the proper boardSquares item.
           scope.boardSquares[index] = scope.currentPlayer;
 
@@ -56,18 +65,41 @@ angular.module('myApp.ticTacToeDirective', ['ngRoute'])
         scope.resetBoard = function() {
           scope.boardSquares = angular.copy(initSquares);
           scope.currentPlayer = 'x';
-          scope.gameOver = undefined; // undefined if game is still playing, 'win' if won, 'tie' if there is no winner
-          scope.errorMsg = undefined; // undefined if no errors, set to error message like 'illegal move' when you click on a square that already has a value
+          scope.gameOver = undefined;
         };
 
         // This function returns true if the current player has won the game
         scope.checkForWin = function() {
+          var board = scope.boardSquares,
+            player = scope.currentPlayer;
+
+          // There are eight possible ways to win - three across, three down and two diagonal
+          // We test for each case
+          if ((board[0] === player && board[1] === player && board[2] === player) ||
+            (board[3] === player && board[4] === player && board[5] === player) ||
+            (board[6] === player && board[7] === player && board[8] === player) ||
+            (board[0] === player && board[3] === player && board[6] === player) ||
+            (board[1] === player && board[4] === player && board[7] === player) ||
+            (board[2] === player && board[5] === player && board[8] === player) ||
+            (board[0] === player && board[4] === player && board[8] === player) ||
+            (board[2] === player && board[4] === player && board[6] === player)) {
+            return true;
+          }
           return false;
         };
 
         // This function returns true if all squares have been chosen
         scope.checkForTie = function() {
-          return false;
+          var board = scope.boardSquares,
+            i,
+            openSquareFound = false;
+
+          for (i = 0; i < 9; i++) {
+            if (board[i] === undefined) {
+              return false;
+            }
+          }
+          return true;
         }
 
         // initialize board now
